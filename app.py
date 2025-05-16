72199c9d9ec93569ac8668878ad7051ace32f93b
Vinith kumar <156978506+VinithKU1@users.noreply.github.com>
2025-05-17 01:22:50 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
-
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -42,13 +42,14 @@ def convert():
         "original_amount": amount,
         "converted_amount": round(converted_amount, 2)
     })
-
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
+ 
