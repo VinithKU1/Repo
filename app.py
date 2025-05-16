caa227e404f0679c4a6bba3e4387c9cd4d048bf6
Vinith kumar <156978506+VinithKU1@users.noreply.github.com>
2025-05-17 02:03:33 +0530

    app.py

--- app.py
+++ app.py
@@ -44,5 +44,5 @@ def convert():
 
 # ✅ Correct single app.run block
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
